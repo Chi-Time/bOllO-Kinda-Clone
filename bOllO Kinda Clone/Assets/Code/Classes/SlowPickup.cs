296cf4bbd486c63858deeecacc49830d5570acdc
[Guardevoir] <music1384@yahoo.co.uk>
2016-10-16 23:35:16 +0100

    Created slowdown pickup.

---
+++ bOllO Kinda Clone/Assets/Code/Classes/SlowPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SlowPickup : MonoBehaviour
+{
+	private float _SpeedDecrease = 2f;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.CompareTag ("Player"))
+		{
+			var p = other.GetComponent<PlayerController> ();
+			p.Speed -= _SpeedDecrease;
+
+			Destroy (this.gameObject);
+		}
+	}
+}

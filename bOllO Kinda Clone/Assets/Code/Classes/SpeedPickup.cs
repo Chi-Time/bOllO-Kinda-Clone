157ab83b29163c300fcea9bb25a5945c4dbd7124
[Guardevoir] <music1384@yahoo.co.uk>
2016-10-16 23:35:28 +0100

    Created speed up picku

---
+++ bOllO Kinda Clone/Assets/Code/Classes/SpeedPickup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedPickup : MonoBehaviour
+{
+	private float _SpeedIncrease = 2f;
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject.CompareTag ("Player"))
+		{
+			var p = other.GetComponent<PlayerController> ();
+			p.Speed += _SpeedIncrease;
+
+			Destroy (this.gameObject);
+		}
+	}
+}
